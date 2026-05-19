3b2b3b0ad2f2f723f0cc1efbf040f99b4a0a416b
hkanki <hirakan22@icloud.com>
2026-05-19 19:08:16 +0900

    officeデータセットのシェルを自分の環境で実行できるように変更した

--- script/office31.sh
+++ script/office31.sh
@@ -1,23 +1,17 @@
 #!/bin/bash
-#SBATCH -J office31
-#SBATCH -p ShangHAI
-#SBATCH --cpus-per-task=6
-#SBATCH --mail-type=all
-#SBATCH --mail-user=YOU@MAIL.COM
-#SBATCH -N 1
-#SBATCH --gres=gpu:1
-#SBATCH --output=output/%j.out
-#SBATCH --error=output/%j.err
-#SBATCH --time 2-00:00
 
+set -e
 
 cd ..
+
 py_main='main'
-gpu=$CUDA_VISIBLE_DEVICES
+gpu=0
 
 dataset='office31'
 domains=(amazon dslr webcam)
 exp=${dataset}
+
+mkdir -p output
 
 for source in ${domains[@]}
 do
@@ -25,7 +19,8 @@ do
     do
         if [[ "${source}" != "${target}" ]]
         then
-            python ${py_main}.py --gpu ${gpu} --exp ${exp} --dataset ${dataset} --source ${source} --target ${target}
+            echo "Running ${source} -> ${target}"
+            python3 ${py_main}.py --gpu_index ${gpu} --exp ${exp} --dataset ${dataset} --source ${source} --target ${target}
         fi
     done
 done
